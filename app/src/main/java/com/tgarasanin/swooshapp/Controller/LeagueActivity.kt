96b33bc01c563193d7cec067e9af5a81eea8940b
Teodora Garasanin <t.garasanin@levi9.com>
2020-04-04 17:04:40 +0200

    fix: remove comments

--- app/src/main/java/com/tgarasanin/swooshapp/Controller/LeagueActivity.kt
+++ app/src/main/java/com/tgarasanin/swooshapp/Controller/LeagueActivity.kt
@@ -2,6 +2,7 @@ package com.tgarasanin.swooshapp.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.tgarasanin.swooshapp.Model.Player
@@ -11,39 +12,46 @@ import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
-//    var selectedLeague = ""
     var player = Player("", "")
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(EXTRA_PLAYER, player)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable<Player>(EXTRA_PLAYER)!!
+        }
+    }
+
     fun onMensClicked(view: View) {
         womensToggleButton.isChecked = false
         coEdToggleButton.isChecked = false
-//        selectedLeague = "mens"
         player.league = "mens"
     }
 
     fun onWomensClicked(view: View) {
         mensToggleButton.isChecked = false
         coEdToggleButton.isChecked = false
-//        selectedLeague = "womens"
         player.league = "womens"
     }
 
     fun onCoEdClicked(view: View) {
         mensToggleButton.isChecked = false
         womensToggleButton.isChecked = false
-//        selectedLeague = "co-ed"
         player.league = "co-ed"
     }
 
     fun nextButtonClicked(view: View) {
         if (player.league != "") {
             val skillActivity = Intent(this, SkillActivity::class.java)
-//            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
             skillActivity.putExtra(EXTRA_PLAYER, player)
             startActivity(skillActivity)
         } else {
