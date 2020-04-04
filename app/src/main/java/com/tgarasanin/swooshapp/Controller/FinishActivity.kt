f772371155dc51ef186840595d27cd7eed7773e8
Teodora Garasanin <t.garasanin@levi9.com>
2020-04-04 17:04:19 +0200

    feat: add onSaveInstanceState/onRestore

--- app/src/main/java/com/tgarasanin/swooshapp/Controller/FinishActivity.kt
+++ app/src/main/java/com/tgarasanin/swooshapp/Controller/FinishActivity.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import com.tgarasanin.swooshapp.Model.Player
 import com.tgarasanin.swooshapp.R
 import com.tgarasanin.swooshapp.Utilities.EXTRA_PLAYER
-//import com.tgarasanin.swooshapp.Utilities.EXTRA_LEAGUE
-//import com.tgarasanin.swooshapp.Utilities.EXTRA_SKILL
 import kotlinx.android.synthetic.main.activity_finish.*
 
 class FinishActivity : BaseActivity() {
@@ -14,10 +12,6 @@ class FinishActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
-
-//        val league = intent.getStringExtra(EXTRA_LEAGUE)
-//        val skill = intent.getStringExtra(EXTRA_SKILL)
-
         val player = intent.getParcelableExtra<Player>(EXTRA_PLAYER)
 
         searchLeaguesText.text = "Looking for ${player.league} ${player.skill} league near you ..."
--- app/src/main/java/com/tgarasanin/swooshapp/Controller/SkillActivity.kt
+++ app/src/main/java/com/tgarasanin/swooshapp/Controller/SkillActivity.kt
@@ -6,29 +6,23 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.tgarasanin.swooshapp.Model.Player
-//import com.tgarasanin.swooshapp.Utilities.EXTRA_LEAGUE
 import com.tgarasanin.swooshapp.R
 import com.tgarasanin.swooshapp.Utilities.EXTRA_PLAYER
-//import com.tgarasanin.swooshapp.Utilities.EXTRA_SKILL
 import kotlinx.android.synthetic.main.activity_skill.*
 
 class SkillActivity : BaseActivity() {
 
-//    var league = ""
-//    var skill = ""
+
     lateinit var player: Player
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
-//        league = intent.getStringExtra(EXTRA_LEAGUE)
         player = intent.getParcelableExtra(EXTRA_PLAYER)
-//        Log.d("SKILL", league)
     }
 
     fun onBeginnerClicked(view: View) {
         ballerToggleButton.isChecked = false
-//        skill = "beginner"
         player.skill = "beginner"
         Log.d("SKILL", "on beginner clicked")
     }
@@ -44,12 +38,22 @@ class SkillActivity : BaseActivity() {
         if (player.skill != "") {
             var finishActivity = Intent(this, FinishActivity::class.java)
             finishActivity.putExtra(EXTRA_PLAYER, player)
-//            finishActivity.putExtra(EXTRA_LEAGUE, league)
-//            finishActivity.putExtra(EXTRA_SKILL, skill)
             startActivity(finishActivity)
 
         } else {
             Toast.makeText(this, "Please select a skill level", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(EXTRA_PLAYER, player)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable<Player>(EXTRA_PLAYER)!!
         }
     }
 
